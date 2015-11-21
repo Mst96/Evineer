5741d3121192288c614b2f98aa4c6deda6018995
GoAwayPeter <gaw.undefined@gmail.com>
2015-11-21 23:53:03 +0000

    alsdkjhf

--- php/register.php
+++ php/register.php
@@ -11,8 +11,10 @@
             die("&#8226 Please enter an email<br>");
 
         if(empty($_POST['role']))
+        {
             $mes = $_POST['role'];
             die("&#8226 Please enter an role:$mes<br>");
+        }
 
         elseif(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
             die("&#8226 Invalid email address<br>");
