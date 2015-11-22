48924d2944aa4b34318fc078ee44c7c879e1c942
Mst96 <Mustafa.96@hotmail.co.uk>
2015-11-22 10:45:57 +0000

    loading wheel

--- js/common.js
+++ js/common.js
@@ -55,8 +55,35 @@ function submitPOST(data,uri,response)
  */
 function submitRegistration()
 {
+    var $red = document.querySelector('.red');
+  var $green = document.querySelector('.green');
+  var $blue = document.querySelector('.blue');
+  var $purple = document.querySelector('.purple');
+  var start = 0;
+
+  var tl = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 0.05,
+  });
+  // center
+  tl.fromTo($green, 0.4, {x: 0}, {x: 30, ease: Linear.easeNone}, start);
+  tl.fromTo($blue, 0.4, {x: 0}, {x: -30, ease: Linear.easeNone}, start);
+  // ext 1st
+  tl.to($green, 0.4, {x: 60, ease: Linear.easeNone}, start + 0.45);
+  tl.fromTo($purple, 0.4, {x: 0}, {x: -30, ease: Linear.easeNone}, start + 0.45);
+  tl.to($blue, 0.4, {x: -60, ease: Linear.easeNone}, start + 0.45);
+  tl.fromTo($red, 0.4, {x: 0}, {x: 30, ease: Linear.easeNone}, start + 0.45);
+  // ext return
+  tl.to($green, 0.4, {x: 30, ease: Linear.easeNone}, start + 0.9);
+  tl.to($purple, 0.4, {x: 0, ease: Linear.easeNone}, start + 0.9);
+  tl.to($blue, 0.4, {x: -30, ease: Linear.easeNone}, start + 0.9);
+  tl.to($red, 0.4, {x: 0, ease: Linear.easeNone}, start + 0.9);
+  // center again
+  tl.to($green, 0.4, {x: 0, ease: Linear.easeNone}, start + 1.35);
+  tl.to($blue, 0.4, {x: 0, ease: Linear.easeNone}, start + 1.35);
     var email = document.getElementById("email").value;
     var role = document.getElementById("role").value;
+    
     console.log(role);
     document.getElementById("errors").innerHTML = "";
     submitPOST("email=" + email + "&role=" + role,"php/register.php","errors");
